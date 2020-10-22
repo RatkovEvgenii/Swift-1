3c8e7c189e36f7eaa89b40a7577d8309dfb7412a
Evgenii Ratkov <ratkov.jeka@gmail.com>
2020-10-22 23:16:26 +0300

    First commit

---
+++ lesson 1/1L_RE.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+// exercise 1
+let a: Double = 6
+let b: Double = 1
+let c: Double = -1
+let D: Double = b * b - 4 * a * c
+let x1: Double = ( -b - sqrt(D)) / 2 * a
+let x2: Double = ( -b + sqrt(D)) / 2 * a
+
+// exercise 2
+
+let cathA: Double = 3
+let cathB: Double = 4
+let hypotenuse = sqrt(cathA * cathA + cathB * cathB)
+let perimetr = cathA + cathB + hypotenuse
+let area = cathA * cathB / 2
+
+// exercise 3
+
+let amount: Double  = 1000
+let percent: Double  = 3
+let years: Double  = 5
+let totalamount: Double  = amount * (1 + percent * years / 100)
